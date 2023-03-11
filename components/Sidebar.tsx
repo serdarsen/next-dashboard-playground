2d320fb4c5c796b663ed94de1919681562dd3efb
serdarsen <serdarsendev@gmail.com>
2023-03-11 18:20:52 +0300

    Add sidebar icons.

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,5 +1,11 @@
 import Link from "next/link";
-import { RxSketchLogo } from "react-icons/rx";
+import { FiSettings } from "react-icons/fi";
+import { HiOutlineShoppingBag } from "react-icons/hi";
+import {
+  RxDashboard,
+  RxPerson,
+  RxSketchLogo,
+} from "react-icons/rx";
 
 interface Prop {
   children: React.ReactNode;
@@ -7,11 +13,32 @@ interface Prop {
 
 const Sidebar = ({ children }: Prop) => (
   <div className="flex dark:bg-slate-800 dark:text-gray-300">
-    <div className="fixed flex h-screen w-20 flex-col justify-between border-r p-4 dark:border-slate-700">
+    <div className="fixed flex h-screen w-20 flex-col justify-between border-r border-gray-200 p-4 dark:border-slate-700">
       <div className="flex flex-col items-center">
         <Link href="/">
           <div className="inline-block rounded-lg bg-purple-800 p-3 text-white dark:text-gray-300">
             <RxSketchLogo size={20} />
+          </div>
+        </Link>
+        <span className="w-full border-b border-gray-200 p-2 dark:border-slate-600"></span>
+        <Link href="/">
+          <div className="my-4 inline-block cursor-pointer rounded-lg bg-gray-100 p-3 hover:bg-gray-200 dark:bg-slate-700 dark:text-gray-300 dark:hover:bg-slate-600">
+            <RxDashboard size={20} />
+          </div>
+        </Link>
+        <Link href="/customer">
+          <div className="my-4 inline-block cursor-pointer rounded-lg bg-gray-100 p-3 hover:bg-gray-200 dark:bg-slate-700 dark:text-gray-300 dark:hover:bg-slate-600">
+            <RxPerson size={20} />
+          </div>
+        </Link>
+        <Link href="/orders">
+          <div className="my-4 inline-block cursor-pointer rounded-lg bg-gray-100 p-3 hover:bg-gray-200 dark:bg-slate-700 dark:text-gray-300 dark:hover:bg-slate-600">
+            <HiOutlineShoppingBag size={20} />
+          </div>
+        </Link>
+        <Link href="/">
+          <div className="my-4 inline-block cursor-pointer rounded-lg bg-gray-100 p-3 hover:bg-gray-200 dark:bg-slate-700 dark:text-gray-300 dark:hover:bg-slate-600">
+            <FiSettings size={20} />
           </div>
         </Link>
       </div>
